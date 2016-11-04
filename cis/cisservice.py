67298b1fe0ab1ac43805569133678071a92c5674
keshav <challa.keshav@gmail.com>
2016-11-04 12:54:52 -0700

    added cis test cases

--- cis/cisservice.py
+++ cis/cisservice.py
@@ -7,52 +7,49 @@ import pickle
 import sys
 from time import gmtime, strftime
 #from requests.packages.urllib3 import response
-#import configs.config
+import configs.config
 
 HEADER = {'content-type': 'text/xml'}
 
 class CISService(unittest.TestCase):
     def __init__(self, testName, *args):
         super(CISService, self).__init__(testName)
-        self.cisEndpoint = "http://weather-citystate-service.cfapps.io/getZipForCityState?state=CT&city=Windsor"
+        self.cisEndpoint = configs.config.CISEndpoint
         logging.basicConfig(filename="cis.log",level=logging.DEBUG)
         sys.stdout = open('finaltestresult', 'w')
-        self.terminal = sys.stdout
         
     def setUp(self):
         logging.debug("Validating: %s"%self._testMethodName)
         logging.debug("Validating: %s"%self.cisEndpoint)
 
     def tearDown(self):
-        logging.debug(" Request:")#%s"%self.cisEndpoint)#, "/%s"%self.params)
-        logging.debug(" Response:")#%s"%self.response)
+        logging.debug(" Request:%s"%self.cisEndpoint)
+        logging.debug(" Response:%s"%self.response)
 
-    def testGetCustomerForValidTN(self):
-        """TestCase:getCustomerForValidTN"""
-        trackingId = "CIS_"+self._testMethodName[4:]+str(time.time()).replace(".","")[8:]
-        self.testId = 1#self.params[0][0]
-        uri = "http://weather-citystate-service.cfapps.io/getZipForCityState?state=CT&city=Windsor"
+    def testValidTN_MultipleCustomers_Success(self):
+        """TestCase:getValidTN_MultipleCustomers_Success"""
+        self.testId = "cis_test_007_Success"
+        uri = configs.config.CISEndpoint
         response = requests.get(uri)
         self.response = response
-        data = response.json()
-        f = open('workfile', 'a')
+        f = open('workfile', 'w')
         pickle.dump(response.content, f)
         f.close()
-        self.assertEqual(data['country abbreviation'], 'US')
+        self.assertEqual(response[1]['tnType'], 'UNKNOWN')
+        self.assertEqual(len(response[0]), 2)   
         
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
-
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
-
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)    
+    def testValidTN_MultipleCustomers_Failure(self):
+        """TestCase:getValidTN_MultipleCustomers_Failure"""
+        self.testId = "cis_test_007_Failure"
+        uri = configs.config.CISEndpoint
+        response = requests.get(uri)
+        self.response = response
+        f = open('workfile', 'w')
+        pickle.dump(response.content, f)
+        f.close()
+        self.assertFalse(len(response[0]), 1)  
         
 if __name__ == '__main__':
+    #del sys.argv[1:]
+    #print sys.argv[1]
     unittest.main()
--- configs/config.py
+++ configs/config.py
@@ -1,29 +1,5 @@
-##############################################
-# Automation Framework Properties
-#
-##############################################
-
-#ACL
-ACL_QA=""""""
-ACL_PROD=""""""
-
-# PyATF DB Properties
-PyATFDBHost=""
-PyATFDBSchema=""
-PyATFDBUserName=""
-PyATFDBPassword=""
-
-#OAuth Endpoints
-CISServiceURL=""
-
-#Timeouts
-BUILD_FETCH_MAXDURATION = 20
-
-#ApplicationHost
-HOST=""
-
 #Endpoints
-CISEndpoint = "http://services.groupkt.com/country/get/iso2code/US"
 GreetingsUrl="http://localhost:8080/"
 WeatherDetailsServiceURL="http://localhost:8080/weatherDetails"
 CityStateService="http://weather-citystate-service.cfapps.io/getZipForCityState?state=CT&city=Windsor"
+CISEndpoint = "https://customer-identity-service-dev1.u1.app.cloud.comcast.net/customeridentity/lookup/tn/6105555551"
